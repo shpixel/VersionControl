670531660cfd4f721f368317bd83512547a7bada
TAMAS-PC\Tamas <tamas.rbskft@gmail.com>
2022-10-01 11:22:15 +0200

    név hozzáadás működik button1-gyel

--- UserMaintenance/Form1.cs
+++ UserMaintenance/Form1.cs
@@ -7,17 +7,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Entities;
 
 namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
+
         public Form1()
         {
             InitializeComponent();
 
             label1.Text = Resource1.LastName;
             label2.Text = Resource1.FirstName;
+
+            listBox1.DisplayMember = "FullName";
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            User user = new User()
+            {
+                FirstName = textBox1.Text,
+                LastName = textBox2.Text
+            };
+
+            users.Add(user);
         }
     }
 }
